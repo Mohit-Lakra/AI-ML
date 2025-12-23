287bf24f4a5ca0b8d3c8891001a5f2719d7b85dc
Mohit-Lakra <mohitlakra2007777@gmail.com>
2025-12-23 23:23:44 +0530

    Learns about strip and cases in string

---
+++ Phase-1/Python/Introduction/string.py
@@ -0,0 +1,11 @@
+a = "    hello world      "
+
+print(a.strip())  # removes whitespace from both ends
+print(a.lstrip()) # removes whitespace from the left end
+print(a.rstrip()) # removes whitespace from the right end
+
+print(name.capitalize()) # Capitalizes the first letter
+print(name.upper())      # Converts to uppercase
+print(name.lower())      # Converts to lowercase
+print(name.title())      # Converts to title case
+
