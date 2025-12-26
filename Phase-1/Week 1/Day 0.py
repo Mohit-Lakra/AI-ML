d668b9c65be1470ce6b5817039e57e7d6b011348
Mohit-Lakra <mohitlakra2007777@gmail.com>
2025-12-26 22:12:58 +0530

    Day 0

---
+++ Phase-1/Week 1/Day 0.py
@@ -0,0 +1,12 @@
+# Learn basics of python 
+# if else conditions
+# while loop
+# for loop
+# functions
+# lists
+# dictionaries
+# tuples
+# etc. etc. etc.
+
+
+# Basicaly learn class 12th python basics, I already know this
